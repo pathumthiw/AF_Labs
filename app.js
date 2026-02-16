822afb5f863ad17ccf079ccdfde25b51fe879101
pathumthiw <pathumthivanka12asd@gmail.com>
2026-02-16 12:29:59 +0530

    makeHttpReq

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ http.createServer(function (req, res) {
  res.end();
 }).listen(8080);
 
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+ let data = '';
+ resp.on('data', (chunk) => {
+ data += chunk;
+ });
+ resp.on('end', () => {
+ console.log(JSON.parse(data));
+ });
+}).on('error', (err) => {
+ console.log("Error: " + err.message);
+});
